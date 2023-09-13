5cb4c6c9513839442447994856d0e55de69c34e4
Paulius Giniunas <paulius000@gmail.com>
2023-09-13 22:26:12 +0300

    Adding language change via javascript, making some initial files absolete

--- SistersGallery.js
+++ SistersGallery.js
@@ -1,1 +1,34 @@
-// tdb
+// ------------------------------------------------------------------------------
+
+// Language change
+
+const languageEnglish = document.querySelectorAll('.language-english');
+const languageLithuanian = document.querySelectorAll('.language-lithuanian');
+const languageToggle = document.getElementById('div-language-change');
+
+let isLanguageEnglishVisible = true; // Initial state - English
+
+languageToggle.addEventListener('click', () => {
+    if (isLanguageEnglishVisible) {
+        // Switch to Lithuanian
+        languageEnglish.forEach(element => {
+            element.style.display = 'none';
+        });
+        languageLithuanian.forEach(element => {
+            element.style.display = 'block';
+        });
+    } else {
+        // Switch to English
+        languageLithuanian.forEach(element => {
+            element.style.display = 'none';
+        });
+        languageEnglish.forEach(element => {
+            element.style.display = 'block';
+        });
+    }
+
+    // Toggle the state
+    isLanguageEnglishVisible = !isLanguageEnglishVisible;
+});
+
+// ------------------------------------------------------------------------------
