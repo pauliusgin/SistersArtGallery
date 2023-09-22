b48014f46eaed89b2383af1693c93008a42a174d
Paulius Giniunas <paulius000@gmail.com>
2023-09-22 07:31:16 +0300

    Added a hamburger menu for smaller screens, still needs fixing for touchscreens

--- SistersGallery.js
+++ SistersGallery.js
@@ -1,6 +1,6 @@
 // ------------------------------------------------------------------------------
 
-// Language change
+//* Language change
 
 const languageEnglish = document.querySelectorAll('.language-english');
 const languageLithuanian = document.querySelectorAll('.language-lithuanian');
@@ -32,3 +32,52 @@ languageToggle.addEventListener('click', () => {
 });
 
 // ------------------------------------------------------------------------------
+
+//* hamburger menu for smaller screens  
+
+const openButton = document.querySelector(".open-button");
+const closeButton = document.querySelector(".close-button");
+const theMenu = document.querySelector(".ul-top");
+let timeoutId;
+
+// function to show the menu
+function showMenu() {
+    theMenu.classList.add("ul-top-visible"); // opens menu
+    openButton.classList.remove("open-button-visible"); // hides hamburger
+    closeButton.classList.add("close-button-visible"); // shows X
+}
+
+// function to hide the menu
+function hideMenu() {
+    theMenu.classList.remove("ul-top-visible");
+    openButton.classList.add("open-button-visible"); // shows hamburger
+    closeButton.classList.remove("close-button-visible"); // hides X
+}
+
+// shows the menu when hamburger is clicked 
+openButton.addEventListener("click", showMenu);
+openButton.addEventListener("touchstart", showMenu);
+
+// closes the menu when X is clicked
+closeButton.addEventListener("click", hideMenu);
+closeButton.addEventListener("touchstart", hideMenu);
+
+// close the menu when mouse pointer leaves the menu area
+theMenu.addEventListener("mouseleave", () => {
+    timeoutId = setTimeout(hideMenu, 500);
+});
+
+// close the menu when mouse is clicked outside of button or menu area
+document.addEventListener("click", (event) => {
+    if (event.target !== openButton && event.target !== theMenu && event.target !== closeButton) { // TODO wondering about this line
+        hideMenu();
+    }
+});
+
+document.addEventListener("touchstart", (event) => {
+    if (event.target !== openButton && event.target !== theMenu && event.target !== closeButton) {
+        hideMenu();
+    }
+});
+
+// --------------------------------------------------------------------------
